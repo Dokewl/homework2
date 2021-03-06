ab38b2af4372754114328bbe33223f3d8bb7c7aa
Daniel Ma <danielmamail@yahoo.com>
2021-03-05 20:23:45 -0800

    fixed code

--- investment.py
+++ investment.py
@@ -6,10 +6,8 @@ def calculate_apr(principal, interest_rate, years):
 		return False
 	if years < 0:
 		return False
-	i = -1
 	output = 0
 	for i in range(years):
-		i = i + 1
 		output = principal * (1 + interest_rate)**years
 		return output
 
