5122d3955275495b965a61adebf9d720de8d6ff7
Daniel Ma <danielmamail@yahoo.com>
2021-03-05 16:29:34 -0800

    fixed code

--- timing.py
+++ timing.py
@@ -1,11 +1,13 @@
 import time
-current_time = time.time()
 def calculate_time(func):
-	print("Printed immediately")
-	print(current_time)
-	func()
-	print("Printed after sleep")
+	def wrapper():
+		print("Printed immediately")
+		print(current_time)
+		func()
+		print("Printed after sleep")
+	return wrapper
 def sleep_time():
+	current_time = time.time()
 	time.sleep(2)
 sleep_time = calculate_time(sleep_time)
 
