fb6c832cc752a6327e617b28f49fb8cd42f8b802
Daniel Ma <danielmamail@yahoo.com>
2021-03-05 17:15:36 -0800

    fixed code

--- investment.py
+++ investment.py
@@ -1,16 +1,16 @@
 def calculate_apr(principal, interest_rate, years):
-        print("Enter the amount of money you're starting with:")
-        principal = int(input())
-        if (principal < 0):
-                return False
-        print("Enter the rate per year in decimal form:")
-        interest_rate = float(input())
-        if (interest_rate < 0):
-                return False
-        print("Enter the amount of time you are investing the money:")
-        years = int(input())
-        if (years < 0):
-             return False
-        output = ((principal)*(1+interest_rate)**(years))
-        print(f"The value of the investment is {output} after {years} number of years.")
-calculate_apr(principal, interest_rate, years)
+	if principal < 0:
+		return False
+	if interest_rate < 0:
+		return False
+	if years < 0:
+		return False
+	total = 0
+	for i in range(years):
+		total = total + (principal * (1 + interest_rate))
+	if total:
+		print(total)
+	else:
+		total = False
+calculate_apr(principal = int(input()), interest_rate = float(input()), years = int(input()))
+
