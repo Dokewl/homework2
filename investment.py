e80960bfe94e4af73de16a4d5d3d9063ef0cff14
Daniel Ma <danielmamail@yahoo.com>
2021-03-05 20:45:11 -0800

    I fixed my code and added comments/docstring.

--- investment.py
+++ investment.py
@@ -1,5 +1,7 @@
-
 def calculate_apr(principal, interest_rate, years):
+	""" Calculate the value of the investment after a certain amount of years. 
+	Takes principal, interest_rate, and years input and returns output. """
+	# Using if statements to check if inputs are invald with negative numbers
 	if principal < 0:
 		return False
 	if interest_rate < 0:
@@ -8,6 +10,7 @@ def calculate_apr(principal, interest_rate, years):
 		return False
 	output = 0
 	for i in range(years):
+		# equation used to calculate the total investments
 		output = principal * (1 + interest_rate)**years
 		return output
 
