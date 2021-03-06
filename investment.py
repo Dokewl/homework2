2fb4a2212a664beb286bef431ea567cefc24f0ca
Daniel Ma <danielmamail@yahoo.com>
2021-03-05 18:11:13 -0800

    fixed code

--- investment.py
+++ investment.py
@@ -8,7 +8,7 @@ def calculate_apr(principal, interest_rate, years):
 		return False
 	if years < 0:
 		return False
-	if (principal !=  isintance(principal, int):
+	if (principal !=  isinstance(principal, int):
 		return False
 	if (principal != isinstance(principal, float):
 		return False
